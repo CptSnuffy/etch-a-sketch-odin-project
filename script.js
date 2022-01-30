c5474cd4b4f38cad0c59f87a5cccafca35a0b74a
CptSnuffy <jhutchinson.programmer@gmail.com>
2022-01-29 19:52:56 -0500

    Main style added CSS grid made

---
+++ script.js
@@ -0,0 +1,23 @@
+
+function paintCell()
+{
+    EventTarget.style.backgroundColor = 'white';
+}
+
+function setupGame(gridSize = 16)
+{
+
+    let divParent = document.getElementById('gridparent');
+
+    divParent.style.setProperty('--grid-rows', gridSize);
+    divParent.style.setProperty('--grid-cols', gridSize);
+
+    for(i = 0; i < (gridSize * gridSize); i++)
+    {
+        let cell = document.createElement('div');
+        cell.addEventListener("mouseover", paintCell);
+        divParent.appendChild(cell).className="sketchdiv"; 
+    }
+}
+
+setupGame();
